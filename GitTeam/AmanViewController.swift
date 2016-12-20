0fa33f59ed2c18147d09a8bfcc9e84a1b4bf7239
Aman <amsingh714@gmail.com>
2016-12-20 14:26:43 -0500

    add storyboard and vc

---
+++ GitTeam/AmanViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AmanViewController.swift
+//  GitTeam
+//
+//  Created by Aman Singh on 12/20/16.
+//  Copyright © 2016 jay. All rights reserved.
+//
+
+import UIKit
+
+class AmanViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
