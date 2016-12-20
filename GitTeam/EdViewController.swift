16946c53725ef87c9d418240f9968332e2c1d644
kobst <edwrdhn@gmail.com>
2016-12-20 15:09:35 -0500

    my view ontroller

---
+++ GitTeam/EdViewController.swift
@@ -0,0 +1,50 @@
+//
+//  EdViewController.swift
+//  GitTeam
+//
+//  Created by Edward Han on 12/20/16.
+//  Copyright © 2016 jay. All rights reserved.
+//
+
+import UIKit
+
+
+class EdViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.black
+        
+        imageView.layer.masksToBounds = true
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.image = #imageLiteral(resourceName: "Image")
+        imageView.alpha = 0
+    
+        
+        imageView.layer.cornerRadius = imageView.layer.borderWidth / 5
+        
+        UIView.animate(withDuration: 5.0, delay: 2, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.imageView.alpha = 1.0
+        }, completion: nil)
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
